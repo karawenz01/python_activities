912d8391747a12ab97341b51ca64b86598c62fee
KWenz <karawenz01@gmail.com>
2018-05-05 11:16:57 -0700

    second activity

---
+++ 2_activity/dictionaries.py
@@ -0,0 +1,15 @@
+lst = ["A", "B", "C"]
+
+item = {"key": 100}
+
+item2 = {"name": "Kara"}
+print(item["key"])
+print(item2["name"])
+
+#dict can contain mult pairs of info
+hero = {"name": "Iron Man", "Nationality": "United States"}
+
+item3 = {"bag": ["laptop", "usb", "food"], "pockets": [5.00, 1.00, gum], "reddit": {"key": [1,2,3,4]}}
+
+print(item3["bag"])
+print(item3['reddit']['key'][3])
